60e05873dba83b93d2ee1e37bef60f82780cc36f
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-28 23:41:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Todo from "./components/Todo.jsx";
 export default function App() {
   return (
     <div className="app"> {/* Main application */}
-      <h1 className="app-heading">Todo App</h1>
+      <h1 className="app-heading">Todojo</h1>
       <Todo /> {/* Render the Todo component */}
     </div>
   );
