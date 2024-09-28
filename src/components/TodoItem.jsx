fc8750b0b27849ca9b2177dfbb83b54b7b239e14
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-28 19:56:40 +0530

    Update TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -9,17 +9,17 @@ export default function TodoItem({ todo, deleteTodo, completeTodo }) {
             : {}
         }
       >
-        {todo.task}
+        {todo.task} {/* Displaying the todo task */}
       </span>
       <button className="rmv-btn btn" onClick={() => deleteTodo(todo.id)}>
-        delete
+        delete {/* Button to delete the todo */}
       </button>
       <button
-        style={todo.isDone ? { display: "none" } : {}}
+        style={todo.isDone ? { display: "none" } : {}} // Hide button if task is completed
         className="cmplt-btn btn"
         onClick={() => completeTodo(todo.id)}
       >
-        complete
+        complete {/* Button to mark the todo as completed */}
       </button>
     </li>
   );
