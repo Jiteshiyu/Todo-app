b5aa9cfcd9c1fa82ea44e620fe13a9e4865b297c
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-28 20:25:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       external: ['uuid']
+    }
+  },
+  resolve: {
+    alias: {
+      'uuid': path.resolve(__dirname, 'node_modules/uuid/dist/esm-browser/index.js'),
     }
   }
 })
