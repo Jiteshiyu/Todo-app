116d68567dce8d0871a41dbb0762f1ca83ee9668
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-10-01 01:59:58 +0530

    Update TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,14 +3,14 @@ import TodoItem from "./TodoItem.jsx";
 export default function TodoList({ todos, deleteTodo, completeTodo }) {
   return (
     <div>
-      <h2>Tasks to do:</h2>
-      <ul>
+      <h2 className="list-title">Tasks to do:</h2>
+      <ul className="todo-list">
         {todos.map((todo) => (
           <TodoItem
             key={todo.id}
             todo={todo}
-            deleteTodo={() => deleteTodo(todo.id)} // Function to delete the todo
-            completeTodo={() => completeTodo(todo.id)} // Function to complete the todo
+            deleteTodo={() => deleteTodo(todo.id)}
+            completeTodo={() => completeTodo(todo.id)}
           />
         ))}
       </ul>
