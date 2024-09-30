98c7fe751f4418707b703c0f748ddd43a0e2bbf2
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-30 22:06:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Todo from "./components/Todo.jsx";
 export default function App() {
   return (
     <div className="app-container"> {/* Main application */}
-      <div className="app-img"> {/* Main application */}
-        <img src="https://plus.unsplash.com/premium_photo-1681487870238-4a2dfddc6bcb" alt="todo-img"></img>
+      <div> {/* Main application */}
+        <img src="https://plus.unsplash.com/premium_photo-1681487870238-4a2dfddc6bcb" alt="todo-img"  className="app-img"></img>
       </div>
       <div className="app"> {/* Main application */}
         <h1 className="app-heading">Todojo</h1>
