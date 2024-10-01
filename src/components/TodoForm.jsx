73db8ebb1bbc225d037051b218f9cf3f2c9e7c39
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-10-02 01:03:16 +0530

    Update TodoForm.jsx

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -4,6 +4,7 @@ export default function TodoForm({ addNewTodo }) {
   // State to hold the new todo input
   let [newTodo, setNewTodo] = useState("");
 
+  // Function to add new todo
   let updateTodoValue = (event) => {
     event.preventDefault();
     if (!newTodo) return;
@@ -12,16 +13,19 @@ export default function TodoForm({ addNewTodo }) {
   };
 
   return (
+    // Todo input form
     <form onSubmit={updateTodoValue}>
       <input
         className="searchbox"
         type="text"
         value={newTodo}
         placeholder=" Add a new todo"
-        onChange={(event) => setNewTodo(event.target.value)} // Update state on input change
+        onChange={(event) => setNewTodo(event.target.value)}
+        // Update state on input change
       />
+      {/* Button to submit the new todo */}
       <button className="submit-btn btn" type="submit">
-        Add Todo {/* Button to submit the new todo */}
+        Add Todo
       </button>
     </form>
   );
