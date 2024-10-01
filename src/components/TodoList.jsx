4e0f3636a59fd3850a04aed5b67197485b57fcd0
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-10-02 01:07:03 +0530

    Update TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,10 +3,15 @@ import TodoItem from "./TodoItem.jsx";
 export default function TodoList({ todos, deleteTodo, completeTodo }) {
   return (
     <div>
+      {/* Title for the todo list */}
       <h2 className="list-title">Tasks to do:</h2>
+
+      {/* Render the list of todos */}
       <ul className="todo-list">
+        {/* Map through the todos array and render a TodoItem for each todo */}
         {todos.map((todo) => (
           <TodoItem
+            // Provide a unique key for each item
             key={todo.id}
             todo={todo}
             deleteTodo={() => deleteTodo(todo.id)}
